bf4043e334276e6502456dd33b0c54391e9c0cca
Luanashinsato <lshinsato@usf.edu>
2025-02-15 21:26:39 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -7,3 +7,13 @@ function calculateInvoice(subtotal, taxRate, discount) {
 // Testing the function 
 calculateInvoice(100, 0.08, 5);
 calculateInvoice(500, 0.1, 20);
+
+// Task 2: Function Expression
+// Writing a function expression that calculates hourly wage
+function calculateHourlyWage(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);
+}
+// Testing the function
+calculateHourlyWage(52000, 40); 
+calculateHourlyWage(75000, 35); 
